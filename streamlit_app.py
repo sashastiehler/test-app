b9d6d6401daa87d7f06c07fe70e3d32ef0d517b1
Sasha Stiehler <129964275+sashastiehler@users.noreply.github.com>
2025-05-23 10:19:20 +0000

    testapp v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
+import mag4 as mg
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Title")
+
+st.write('Hello World')
+
+df = mg.get_data('bastcrat')
+
+xEl,yEl = df['Mg'],df['Si']
+
+elements = df.columns.tolist()[27:]
+xEl = st.selectbox("Select First Element",elements,0)
+yEl = st.selectbox("Select Second Element",elements,9)
+
+fig, ax = plt.subplots()
+ax.scatter(df[xEl],df[yEl])
+ax.set_xlabel(f'{xEl} (w%)')
+ax.set_ylabel(f'{yEl} (w%)')
+st.pyplot(fig)
