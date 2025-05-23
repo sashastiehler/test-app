8bab2b847451736bcc53d174c1ff4f4774b08412
Sasha Stiehler <129964275+sashastiehler@users.noreply.github.com>
2025-05-23 10:58:19 +0000

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,14 @@ df_georoc_datasets = df_mag4_available[fil]['Title'].tolist()
 
 locals = st.selectbox("Select Location",df_georoc_datasets,0)
 
-df = mg.get_data('bastcrat')
-
-xEl,yEl = df['Mg'],df['Si']
+df = mg.get_data(locals)
 
 elements = df.columns.tolist()[27:]
 xEl = st.selectbox("Select First Element",elements,0)
 yEl = st.selectbox("Select Second Element",elements,9)
 
 fig, ax = plt.subplots()
-ax.scatter(df[xEl],df[yEl])
+ax.scatter(df[xEl]/10000,df[yEl]/10000)
 ax.set_xlabel(f'{xEl} (w%)')
 ax.set_ylabel(f'{yEl} (w%)')
 st.pyplot(fig)
